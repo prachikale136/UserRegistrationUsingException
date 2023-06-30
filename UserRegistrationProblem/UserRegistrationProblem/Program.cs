3126811914b3e05d756123812e0c3186cc501d5c
Prachi <prachikale136@gmail.com>
2023-06-30 11:02:27 +0530

    [Prachi] Add: VAlidate string for first name

---
+++ UserRegistrationProblem/UserRegistrationProblem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    internal class Program
+    {
+        public static void Main(string[] args) 
+        {
+
+            UserRegistration validateUser = new UserRegistration();
+            validateUser.validateFirstName("Prachi");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UserRegistrationProblem/UserRegistrationProblem/UserRegistration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    internal class UserRegistration
+    {
+        //For First NAme
+        public void validateFirstName(string firstName)
+        {
+            string stringForFirstName = "^[A-Z][a-z]{3,}?";
+            try
+            {
+
+                if (Regex.IsMatch(firstName, stringForFirstName))
+                    Console.WriteLine(firstName + " is Valid");
+
+                else
+                    Console.WriteLine(firstName + " is Invalid");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }  
+    }
+}
